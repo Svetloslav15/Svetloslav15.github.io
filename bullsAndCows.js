ccdbe35f1145a75c8435c8046fb7df9016603258
Svetloslav15 <sn_novoselski@abv.bg>
2018-07-16 20:52:20 +0300

    add changes

--- bullsAndCows.js
+++ bullsAndCows.js
@@ -9,38 +9,36 @@ let movesCounter = (function () {
     return {restart, count};
 }());
 
+$( "#yourNum" ).on( "keydown", function(event) {
+    if(event.which == 13){
+        calculateBullsAndCows();
+    }
+});
+
 function generateNum(number) {
-    let num = Math.floor(Math.random() * 8998) + 1000;
+    let num = Math.floor(Math.random() * 9999) + 1000;
     $('#secretNum').val(num);
     $('#firstSection  h1').css("margin-right", "0");
     if (number === 1) {
         $('#start').css("display", "none");
-        $('#surrender-or-win').css("display", "none");
         $('#new-game').css("display", "block");
         $('#oldMoves').text("");
     }
     else if (number === 2){
         $('#start').css("display", "block");
         $('#new-game').css("display", "none");
-        $('#surrender-or-win').css("display", "none");
-
     }
-    $("#bulls").val("0");
-    $("#cows").val("0");
-    $('#surrender-or-win').text("Числото, което трябваше да познаеш беше ");
-    $('#play').removeAttr("disabled", "enable");
-    $('#surrenderBtn').removeAttr("disabled", "enable");
+    $('#play').prop("disabled", false);
     movesCounter.restart();
 }
 
-
 function calculateBullsAndCows() {
-    $('#surrenderBtn').css("display", "inline-block");
     let numberForGuess = $('#secretNum').val();
     let tryNum = Number($('#yourNum').val()).toString();
 
     if (tryNum === "NaN" || tryNum.length != 4){
-        $('#yourNum').val("Invalid Input!");
+        alert("Invalid Input!");
+        $('#yourNum').val("");
         return;
     }
     let isGuessVisted = [false, false, false, false];
@@ -70,40 +68,28 @@ function calculateBullsAndCows() {
         }
     }
 
-    $('#bulls').val(bulls);
-    $('#cows').val(cows);
     let moves = movesCounter.count();
     let text = `${moves}. ${tryNum} = ${bulls} бика и ${cows} крави\n`;
     $('#oldMoves').append(text);
+    $('#yourNum').val("");
 }
 function surrender() {
     let num = $('#secretNum').val();
-    $('#surrender-or-win').text(`Числото, което трябваше да познаеш е ${num}!`);
-    $('#surrender-or-win').fadeIn();
-    $('#surrender-or-win').css("display", "inline-block");
-    $('#play').attr("disabled", "disabled");
-    $('#surrenderBtn').attr("disabled", "disabled");
-    $('#firstSection  h1').css("margin-right", "7em");
+    alert(`Числото, което трябваше да познаеш е ${num}!`);
+    $('#play').prop("disabled", true);
 }
 function win(tryNum) {
-    $('#surrender-or-win').text("Честито ти позна числото искаш ли да играеш отново?");
-    $('#surrender-or-win').fadeIn();
-    $('#surrender-or-win').css("display", "inline-block");
-    $('#firstSection  h1').css("margin-right", "7em");
-    $('#surrenderBtn').attr("disabled", "disabled");
-    $('#play').attr("disabled", "disabled");
+    alert("Честито ти позна числото искаш ли да играеш отново?");
+    $('#play').prop("disabled", true);
     let moves = movesCounter.count();
     let text = `${moves}. ${tryNum} = 4 бика и 0 крави\n`;
     $('#oldMoves').append(text);
 }
 function showRules() {
-    $('#thirdSection > p').fadeIn();
-    $('#thirdSection > p').css("display", "table");
+    alert("Целта на играта е да познаете кое число си е намислил компютъра. " +
+        "Числото ще бъде в диапазона от 1000 до 9999.\nКогато получите \"крава\" " +
+        "означава, че някоя от цифрите на вашето число се съдържа в числото на компютъра,\n" +
+        "но не си е на мястото. Ако получите \"бик\", значи числото си е на мястото. " +
+        "Целта е да получите 4 бика!");
     $('#play').attr("disabled", "disabled");
-    $('#surrenderBtn').attr("disabled", "disabled");
-}
-function closeRules() {
-    $("#thirdSection > p").css("display", "none");
-    $('#play').removeAttr("disabled");
-    $('#surrenderBtn').removeAttr("disabled");
 }
